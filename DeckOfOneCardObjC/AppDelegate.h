3976ba1d895aeb5185da4d27a4aefcc801ef4801
Aaron Shackelford <burntcreamlatte@icloud.com>
2019-12-03 12:01:37 -0700

    Initial Commit

---
+++ DeckOfOneCardObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DeckOfOneCardObjC
+//
+//  Created by Aaron Shackelford on 12/3/19.
+//  Copyright © 2019 Aaron Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
